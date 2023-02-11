aad332912a93d9058349926f2224aeab73d7b9bf
Kotori <ani6376@gmail.com>
2023-02-11 04:18:20 -0500

    Initial commit. Example project containing multiple RuneLite plugin modules.

---
+++ javaexample/src/main/java/com/example/javaexample/JavaExamplePlugin.java
@@ -0,0 +1,59 @@
+package com.example.javaexample;
+
+import com.google.inject.Provides;
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.events.GameTick;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import org.pf4j.Extension;
+
+@Extension
+@PluginDescriptor(
+	name = "Java example",
+	description = "Java example"
+)
+@Slf4j
+public class JavaExamplePlugin extends Plugin
+{
+	// Injects our config
+	@Inject
+	private JavaExampleConfig config;
+
+	// Provides our config
+	@Provides
+	JavaExampleConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(JavaExampleConfig.class);
+	}
+
+	@Override
+	protected void startUp()
+	{
+		// runs on plugin startup
+		log.info("Plugin started");
+
+		// example how to use config items
+		if (config.example())
+		{
+			// do stuff
+			log.info("The value of 'config.example()' is ${config.example()}");
+		}
+	}
+
+	@Override
+	protected void shutDown()
+	{
+		// runs on plugin shutdown
+		log.info("Plugin stopped");
+	}
+
+	@Subscribe
+	private void onGameTick(GameTick gameTick)
+	{
+		// runs every gametick
+		log.info("Gametick");
+	}
+}
