ce087fd539e16ab45318c3a37aaf55c3be5154cf
OreoCupcakes <ani6376@gmail.com>
2023-02-27 13:36:59 -0500

    demonics config

--- demonicgorillas/src/main/java/com/theplug/kotori/demonicgorillas/DemonicGorillaConfig.java
+++ demonicgorillas/src/main/java/com/theplug/kotori/demonicgorillas/DemonicGorillaConfig.java
@@ -2,6 +2,7 @@ package com.theplug.kotori.demonicgorillas;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
 import net.runelite.client.config.ConfigSection;
 
 @ConfigGroup("demonicgorilla")
@@ -9,10 +10,18 @@ public interface DemonicGorillaConfig extends Config
 {
     @ConfigSection(
             name = "<html><div style='text-align:center;padding-left:25px;padding-right:25px;'>Demonic Gorillas<br>" +
-                    "Version 1.1.3</div></html>",
+                    "Version 1.1.4</div></html>",
             description = "",
             position = 0,
             closedByDefault = true
     )
     String versionInfo = "Loader Version";
+
+    @ConfigItem(
+            keyName = "version",
+            name = "",
+            description = "",
+            section = versionInfo
+    )
+    default String versionString() { return "Demonic Gorillas\nVersion 1.1.4"; }
 }
