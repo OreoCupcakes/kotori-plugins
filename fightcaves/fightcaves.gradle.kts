e02d9cbad543b5acd3d1749ba64a1915bbe454eb
Kotori <ani6376@gmail.com>
2023-05-08 00:06:35 -0400

    FightCaves - optimized to minimize memory footprint by clearing lists on plugin shutdown

--- fightcaves/fightcaves.gradle.kts
+++ fightcaves/fightcaves.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "1.0.0"
+version = "1.2.0"
 
 project.extra["PluginName"] = "Fight Caves" // This is the name that is used in the external plugin manager panel
 project.extra["PluginDescription"] = "Displays current and upcoming wave monsters in the Fight Caves and what to pray at TzTok-Jad." // This is the description that is used in the external plugin manager panel
