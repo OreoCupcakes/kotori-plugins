3ad78e780ffd57f7df457cec21683c830a28e685
Kotori <ani6376@gmail.com>
2023-06-05 22:24:23 -0400

    change url

--- kotoriutils/kotoriutils.gradle.kts
+++ kotoriutils/kotoriutils.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "1.2.0"
+version = "1.2.1"
 
 project.extra["PluginName"] = "Kotori Plugin Utils" // This is the name that is used in the external plugin manager panel
 project.extra["PluginDescription"] = "Utilities needed for certain ported plugins to work." // This is the description that is used in the external plugin manager panel
