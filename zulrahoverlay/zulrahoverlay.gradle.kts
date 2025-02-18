2b420b0ec5d0c9d0982fb4ef2579de8f73c92fd5
Kotori <OreoCupcakes@users.noreply.github.com>
2025-02-18 13:46:44 -0500

    Utilize new byIndex function

--- zulrahoverlay/zulrahoverlay.gradle.kts
+++ zulrahoverlay/zulrahoverlay.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "1.2.5"
+version = "1.2.6"
 
 project.extra["PluginName"] = "<html><font color=#6b8af6>[P]</font> Zulrah</html>" // This is the name that is used in the external plugin manager panel
 project.extra["PluginDescription"] = "An overlay for the Zulrah fight." // This is the description that is used in the external plugin manager panel
